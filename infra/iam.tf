808459b702d2bb3473f0872d673e7f7f64556c80
Jose Marmolejos <jose@cleverfranke.com>
2025-02-04 13:36:41 +0100

    add missing policy

--- infra/iam.tf
+++ infra/iam.tf
@@ -20,3 +20,31 @@ resource "aws_iam_policy_attachment" "lambda_exec_policy_attach" {
   roles      = [aws_iam_role.lambda_exec_role.name]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_iam_policy" "dynamodb_policy" {
+  name        = "lambda_dynamodb_policy"
+  description = "IAM policy to allow Lambda functions to access the DynamoDB table for runTestCases"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:Query",
+          "dynamodb:Scan"
+        ],
+        Resource = aws_dynamodb_table.run_test_cases_table.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "dynamodb_policy_attachment" {
+  name       = "lambda_dynamodb_policy_attachment"
+  roles      = [aws_iam_role.lambda_exec_role.name]
+  policy_arn = aws_iam_policy.dynamodb_policy.arn
+}
