3e23d2e1cb93cddba4b4def75e7aa0b80a3f8a0c
Jose Marmolejos <jose@cleverfranke.com>
2025-02-06 15:29:47 +0100

    manage state remotely

--- infra/main.tf
+++ infra/main.tf
@@ -7,6 +7,14 @@ terraform {
       version = ">= 4.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "api-test-service-terraform-state-bucket"
+    key            = "envs/dev/terraform.tfstate"
+    region         = "eu-north-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
